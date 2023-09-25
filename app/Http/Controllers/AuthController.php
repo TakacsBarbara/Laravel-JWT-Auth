2372e6d1f378c62377e0a2a0c260618f1341372c
Barbara Takacs <takacs.barbara97@gmail.com>
2023-09-25 19:59:07 +0200

    Create controllers and test routes in Postman

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|regex:/^(?=.*[A-Z])(?=.*\d).+$/',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+            'phone_number' => $request->input('phone_number'),
+            'address' => $request->input('address'),
+            'position' => $request->input('position')
+        ]);
+
+        return response()->json(['user' => $user, 'msg' => 'User created successfully'], 201);
+    }
+
+    public function login(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function me()
+    {
+        $user = Auth::user();
+        return response()->json(['user' => $user], 200);
+    }
+
+    public function logout()
+    {
+        $token = JWTAuth::parseToken();
+        $token->invalidate();
+
+        return response()->json(['message' => 'Logged out successfully'], 201);
+    }
+}
